3a7c7f785bcaa556d1b2ae7de3fe4b16c54938ed
Matte <matt.j.guyotte@gmail.com>
2025-02-28 06:09:23 -0500

    updated app_setup.sh to create databases properly

--- init_db.sql
+++ init_db.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE insta_notifications;
 
 -- Create a new user with the password placeholder
-CREATE USER ${DB_USER} WITH ENCRYPTED PASSWORD '${DB_PASS}';
+CREATE USER ${DB_USER} WITH ENCRYPTED PASSWORD '${DB_PASS}' CREATEDB LOGIN;
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE insta_notifications TO ${DB_USER};
